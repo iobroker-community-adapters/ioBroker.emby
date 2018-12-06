f7f3f4df0fbd780d2642acedc0b3c5404ee98b0d
Mike <Admin@mikegerst.de>
2018-12-06 17:27:43 +0100

    Init

---
+++ README.md
@@ -0,0 +1,26 @@
+![Logo](admin/emby.png)
+# ioBroker.template
+=================
+
+This adapter is a template for the creation of an ioBroker adapter. You do not need it unless you plan on developing your own adapter.
+
+It includes both code running within iobroker and as vis widget. If you only plan to create a vis widget then you should use the [iobroker.vis-template](https://github.com/ioBroker/ioBroker.vis-template) instead.
+
+## Steps 
+1. download and unpack this packet from github ```https://github.com/ioBroker/ioBroker.template/archive/master.zip```
+  or clone git repository ```git clone --depth=1 https://github.com/ioBroker/ioBroker.template.git```
+
+2. download required npm packets. Write in ioBroker.template directory:
+
+  ```npm install```
+  
+3. set name of this template. Call
+  
+  ```gulp rename --name mynewname --email email@mail.com --author "Author Name"```
+  
+  *mynewname* must be **lower** case and with no spaces.
+
+  If gulp is not available, install gulp globally:
+  
+  ```npm install -g gulp-cli```
+ 
---
+++ main.js
@@ -0,0 +1,97 @@
+'use strict';
+
+var W3CWebSocket = require('websocket').w3cwebsocket;
+const utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
+const adapter = new utils.Adapter('emby');
+var websocket;
+var checkOnline;
+var connection;
+
+// is called when adapter shuts down - callback has to be called under any circumstances!
+adapter.on('unload', function (callback) {
+    try {
+        connection.send('{"MessageType":"SessionsStop", "Data": ""}');
+        websocket.close();
+        checkOnline = null;
+        adapter.log.info('cleaned everything up...');
+        callback();
+    } catch (e) {
+        callback();
+    }
+});
+
+// is called if a subscribed object changes
+adapter.on('objectChange', function (id, obj) {
+    // Warning, obj can be null if it was deleted
+    adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
+});
+
+
+
+adapter.on('stateChange', function (id, state) {
+    if (id && state && !state.ack)
+	{
+		id = id.substring(adapter.namespace.length + 1);
+        adapter.log.info("state change: " + id + " - " + state.val);
+
+        adapter.setState(id, state.val, true);
+//		switch (id)
+//		{
+//			case 'media_state':
+//                
+//                break;
+//        }
+    }
+});
+
+//adapter.on('message', function (obj) {
+//    if (typeof obj === 'object' && obj.message) {
+//        if (obj.command === 'send') {
+//            if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
+//        }
+//    }
+//});
+
+adapter.on('ready', function () {
+    main();
+});
+
+function main() {
+    adapter.subscribeStates('*');
+adapter.log.info("starting websocket");
+    connection = new W3CWebSocket('ws://192.168.0.129:8096?api_key=8306e66875c54b4c816fed315c3cd2e6&deviceId=00001');
+    adapter.log.info("started websocket");
+
+    connection.onopen = webOpen;
+    connection.onerror = webError;
+    connection.onmessage = webMessage;
+}
+
+function webOpen()
+{
+    connection.send('{"MessageType":"SessionsStart", "Data": "2000,2000"}');
+    adapter.log.info("Mit Server verbunden.");
+}
+
+function webError(error)
+{
+    adapter.log.error("Websocket Error : " + error);
+}
+
+function webMessage(e)
+{
+    var data = JSON.parse(e.data);
+
+    for(var i = 0; i < data.Data.length; i++)
+    {
+        var d = data.Data[i];
+        var state = d.PlayState;
+        
+        if(typeof state.MediaSourceId !== 'undefined')
+        {
+            adapter.setState("isPaused", state.IsPaused, true);
+            adapter.setState("isMuted", state.IsMuted, true);
+            adapter.setState("deviceName", d.DeviceName, true);
+        }
+    }
+}
